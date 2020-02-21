3da441caf04136aa19c2c7c2deb7ad89d7c14dcd
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-02-21 18:16:49 -0500

    ATF should be visible on homepage page load

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,5 +1,12 @@
 <script>
+    import { onMount, getContext } from 'svelte';
+    import { key } from '../components/views/BDCAboveTheFoldView.svelte';
 
+    const { openAtf } = getContext(key);
+
+    onMount(() => {
+        openAtf();
+    });
 </script>
 
 <main class="main-content wrapper">
