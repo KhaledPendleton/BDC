781b6d60b637b5ed4efc32a9f87eb97cd152989f
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-02-22 13:14:39 -0500

    Moved magic constants into their own file.

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const COMPANY_EMAIL = 'hello@binghamton.design';
+export const THEME_NAMES = Object.freeze(['positive', 'neutral', 'negative']);
+export const SOCIAL_MEDIA_ACCOUNTS = Object.freeze([
+    { title: 'Twitter', url: 'https://twitter.com' },
+    { title: 'Instagram', url: 'https://instagram.com' },
+    { title: 'YouTube', url: 'https://youtube.com' }
+]);
+
+export default Object.freeze({
+    COMPANY_EMAIL,
+    THEME_NAMES,
+    SOCIAL_MEDIA_ACCOUNTS
+});
