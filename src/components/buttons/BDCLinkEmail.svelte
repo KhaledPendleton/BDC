e6d7b818b6a4a6575aadfdc69ad1ca36cfa2d594
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-02-22 13:10:56 -0500

    Footer markup more semantic. Removed logic for theme manipulation.

---
+++ src/components/buttons/BDCLinkEmail.svelte
@@ -0,0 +1,7 @@
+<script>
+    import BDCLink from './BDCLink.svelte';
+    export let email = '';
+    const address = 'mailto:' + email;
+</script>
+
+<BDCLink href={address}>{email}</BDCLink>
--- src/components/views/BDCFooterView.svelte
+++ src/components/views/BDCFooterView.svelte
@@ -1,92 +1,95 @@
 <script>
-    import BDCThemeButton, { themes } from '../buttons/BDCThemeButton.svelte';
     import BDCCopyrightLabel from '../labels/BDCCopyrightLabel.svelte';
+    import BDCLinkEmail from '../buttons/BDCLinkEmail.svelte';
+    import BDCButton from '../buttons/BDCButton.svelte';
     import BDCLink from '../buttons/BDCLink.svelte';
 
-    const themeButtons = [
-        { title: 'Positive', theme: themes.POSITIVE },
-        { title: 'Neutral', theme: themes.NEUTRAL },
-        { title: 'Negative', theme: themes.NEGATIVE }
-    ];
-
-    const socialMedia = [
-        { title: 'Twitter', url: 'https://twitter.com' },
-        { title: 'Instagram', url: 'https://instagram.com' },
-        { title: 'YouTube', url: 'https://youtube.com' }
-    ];
-
-    const email = 'hello@binghamton.design';
+    export let socialMediaAccounts = [];
+    export let themes = [];
+    export let email = '';
 </script>
 
 <style>
     footer {
         padding-bottom: 1rem;
         display: grid;
-        row-gap: 1rem;
         grid-template-rows: auto;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-areas:
-            "theme theme"
-            "social social"
-            "email copyright";
+        row-gap: 1rem;
     }
 
-    .theme-controls { grid-area: theme; }
-    .copyright { grid-area: copyright; }
-    .social-media { grid-area: social; }
-    .email { grid-area: email; }
-
-    .copyright {
-        justify-self: end;
-    }
-
-    .theme-controls,
-    .social-media {
+    .socials,
+    .theme-controls {
+        margin: 0;
+        padding: 0;
+        list-style: none;
         display: flex;
         justify-content: space-between;
     }
 
-    /* 625px at 16px font-size */
-    @media screen and (min-width: 39.0625em) {
-        footer {
+    .socials li,
+    .theme-controls li {
+        display: inline-block;
+    }
+
+    .socials { grid-area: socials;}
+    .email-container {grid-area: email;}
+    .copyright-container {
+        grid-area: copyright;
+        justify-self: end;
+    }
+
+    .metadata {
+        display: grid;
+        row-gap: 1rem;
+        grid-template-rows: auto;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-areas: 
+            "socials socials"
+            "email copyright";
+    }
+
+    /* 775px at 16px font-size */
+    @media screen and (min-width: 48.4375em) {
+        .metadata {
+            grid-template-rows: auto;
             grid-template-columns: repeat(3, 1fr);
-            grid-template-areas:
-                "theme theme theme"
-                "email copyright social";
+            grid-template-areas: 
+                "email copyright socials";
         }
 
-        .copyright {
-            justify-self: center;
-        }
-        .email { justify-self: initial }
-        .social-media { justify-content: flex-end; }
-
-        .social-media > span:not(:last-child) {
+        .copyright-container {justify-self: center;}
+        .socials {justify-content: flex-end;}
+        .socials li:not(:last-child) {
             margin-right: 1rem;
         }
     }
 </style>
 
 <footer>
-    <section class="theme-controls">
-        {#each themeButtons as themeButton}
-            <BDCThemeButton
-                title={themeButton.title}
-                theme={themeButton.theme}
-            />
-        {/each}
+    <section class="wrapper">
+        <ul class="theme-controls">
+            {#each themes as theme}
+                <li>
+                    <BDCButton handleClick={theme.updateTheme}>
+                        {theme.name}
+                    </BDCButton>
+                </li>
+            {/each}
+        </ul>
     </section>
-    <section class="email">
-        <BDCLink href={'mailto:' + email} external={true}>{email}</BDCLink>
-    </section>
-    <section class="copyright">
-        <BDCCopyrightLabel class="copyright" />
-    </section>
-    <section class="social-media">
-        {#each socialMedia as sm}
-            <span>
-                <BDCLink href={sm.url} external={true}>{sm.title}</BDCLink>
-            </span>
-        {/each}
+    <section class="wrapper">
+        <div class="metadata">
+            <div class="email-container">
+                <BDCLinkEmail {email} />
+            </div>
+            <div class="copyright-container">
+                <BDCCopyrightLabel />
+            </div>
+            <ul class="socials">
+                {#each socialMediaAccounts as account}
+                    <li><BDCLink external={true} href={account.url}>{account.title}</BDCLink></li>
+                {/each}
+            </ul>
+        </div>
     </section>
 </footer>
